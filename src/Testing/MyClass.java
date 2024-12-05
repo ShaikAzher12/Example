afd80c52317d25b58a3e4a572d8c08f63ca58616
ashaik235 <ashaik235@IN-9ZMKZH3>
2024-12-05 10:30:49 +0530

    cmtt

--- src/Testing/MyClass.java
+++ src/Testing/MyClass.java
@@ -5,8 +5,6 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Hello");
-         System.out.println("Alll12");
-         System.out.println("Alll12");
          System.out.println("Alll12345");
 	}
 
