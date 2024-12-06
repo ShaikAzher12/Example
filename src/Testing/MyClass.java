719c6ad857d209014313628c1ddb35c65a4aea04
ashaik235 <ashaik235@IN-9ZMKZH3>
2024-12-06 16:24:43 +0530

    commit1

--- src/Testing/MyClass.java
+++ src/Testing/MyClass.java
@@ -4,9 +4,10 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("Hello");
+         System.out.println("Hello everyone");
          System.out.println("Alll");
          System.out.println("123");
+         System.out.println("session");
 	}
 
 }
